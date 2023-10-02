6a403ce2bd63c73699a54c81ad70a5ee6383974b
niranjan-official <niranjansabarianth1521@gmail.com>
2023-10-02 22:44:04 +0530

    homepage changed, aboutpage added

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,13 +1,25 @@
 // import React from 'react'
 import PageHead from '../../components/PageHead/PageHead'
 import './About.css'
+import about_logo from '../../assets/about-logo.svg'
 
 const About = () => {
   return (
     <section id='about'>
-       <div className='w-full h-full bg-lime-300'>
-          <PageHead name='About GDSC-PRC'/>
-       </div>
+      <div className='w-full h-full flex flex-col' >
+        <PageHead name='Our Goal' />
+        <div className='w-full h-full flex lg:flex-row flex-col-reverse'>
+
+          <div className='lg:w-1/2 lg:h-full w-full p-4 lg:pt-16'>
+            <p className='lg:text-3xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur soluta ut quod architecto, dignissimos iste accusantium enim libero quia sequi optio sapiente blanditiis vitae voluptatem ipsa modi quidem atque ratione. Lorem, ipsum dolor sit amet consectetur adipisicing elit. A incidunt possimus laborum, labore deserunt nobis voluptatem maxime.</p>
+          </div>
+
+          <div className='lg:w-1/2 lg:h-full w-full h-auto'>
+            <img src={about_logo} alt="" className='lg:h-full lg:w-auto lg:relative lg:-top-16 lg:left-10' />
+          </div>
+
+        </div>
+      </div>
     </section>
   )
 }
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,26 +2,17 @@
 import './Home.css'
 import logo from '../../assets/gdsc-logo.png'
 import colors from '../../assets/colors.png'
-import design_1 from '../../assets/design_1.png'
 
 const Home = () => {
   return (
     <section id='home' className='bg'>
       <div className='h-full w-full flex justify-between items-center lg:items-start home'>
 
-        <div className='w-0 lg:w-3/12 h-full flex justify-end items-end'>
-          <img src={design_1} alt="" className='h-5/6 w-auto'/>
-        </div>
-
-        <div className='flex flex-col items-center justify-center lg:justify-start text-center w-full h-full lg:w-6/12 lg:mt-20 pb-28 pl-16 lg:p-0 '>
+        <div className='flex flex-col items-center justify-center lg:justify-start text-center p-2 w-full h-full lg:mt-20 '>
           <img src={logo} alt="" className='w-40 h-auto' />
-          <h1>Google Developers Students Club</h1>
+          <h1>Google Developer Student Clubs</h1>
           <h2 className='pt-3 lg:pt-0'>Providence College of Engineering</h2>
           <img src={colors} alt="" className='w-36 h-auto mt-2'/>
-        </div>
-
-        <div className='w-0 lg:w-3/12 h-full flex justify-center items-center'>
-          {/* <img src={design_1} alt="" className='h-5/6 w-auto'/> */}
         </div>
 
       </div>
